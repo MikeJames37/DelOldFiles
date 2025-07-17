988ee57426a5b93043f3779ae472c4de92f0608b
MikeJames37 <mikejamesusa37@gmail.com>
2025-07-17 13:48:49 +0300

    feat(script): add remove files older MAX_DAY_LIFE days

--- DelOldFiles/script.py
+++ DelOldFiles/script.py
@@ -3,16 +3,23 @@ from _datetime import datetime, date, time
 import sys
 
 TODAY = datetime.today()
+MAX_DAY_LIFE = 300
 print(TODAY)
 folder_path = r"C:\Кисляков\Скриншоты"
 if os.path.exists(folder_path):
-    print(f"Работаем с папкой: {folder_path}")
     files = os.listdir(folder_path)
+    print(f"Работаем с папкой: {folder_path}, в ней {len(files)} файлов")
     for file in files:
         full_path = os.path.join(folder_path, file)
         date_change = os.path.getmtime(full_path)
-        print(f'{file} create {datetime.fromtimestamp(date_change)}')
-        print(abs(TODAY - datetime.fromtimestamp(date_change)).da)
+        file_day_life = abs(TODAY - datetime.fromtimestamp(date_change)).days
+        print(f'{file} create {datetime.fromtimestamp(date_change)}, {file_day_life} ago')
+        if file_day_life > MAX_DAY_LIFE:
+            print(f'{file} create {datetime.fromtimestamp(date_change)}, {file_day_life} ago')
+            os.remove(full_path)
+            print(len(files))
+    print(f"Работаем с папкой: {folder_path}, в ней {len(files)} файлов")
+
 else:
     print("Папка не найдена!")
 
