28f4f6b9edb2dfbc97819bce615db13595f99524
MikeJames37 <mikejamesusa37@gmail.com>
2025-07-17 17:37:32 +0300

    feat(script): logic on functions

--- DelOldFiles/script.py
+++ DelOldFiles/script.py
@@ -3,24 +3,29 @@ from _datetime import datetime, date, time
 import sys
 
 TODAY = datetime.today()
-MAX_DAY_LIFE = 300
-print(TODAY)
-folder_path = r"C:\Кисляков\Скриншоты"
-if os.path.exists(folder_path):
-    files = os.listdir(folder_path)
-    print(f"Работаем с папкой: {folder_path}, в ней {len(files)} файлов")
-    for file in files:
-        full_path = os.path.join(folder_path, file)
-        date_change = os.path.getmtime(full_path)
-        file_day_life = abs(TODAY - datetime.fromtimestamp(date_change)).days
-        print(f'{file} create {datetime.fromtimestamp(date_change)}, {file_day_life} ago')
-        if file_day_life > MAX_DAY_LIFE:
-            print(f'{file} create {datetime.fromtimestamp(date_change)}, {file_day_life} ago')
-            os.remove(full_path)
-            print(len(files))
-    print(f"Работаем с папкой: {folder_path}, в ней {len(files)} файлов")
-
-else:
-    print("Папка не найдена!")
+DAYS = 7
+FOLDER_PATHS = ["C:\Кисляков\Скриншоты",
+                "C:\Кисляков\Скриншоты—копия",
+                "C:\Кисляков\Screen",]
 
 
+def deleteObjectsInFolder(path: str):
+    if os.path.exists(path):
+        files = os.listdir(path)
+        for file in files:
+            full_path = os.path.join(path, file)
+            date_change = os.path.getmtime(full_path)
+            file_day_life = abs(TODAY - datetime.fromtimestamp(date_change)).days
+            if file_day_life > DAYS:
+                os.remove(full_path)
+    else:
+        print(f"Путь {path} не найден!")
+
+def cleanFolders(folders: list):
+    for path in folders:
+        correct_path = path.replace("\\", "/")
+        print(correct_path)
+        deleteObjectsInFolder(correct_path)
+
+
+cleanFolders(FOLDER_PATHS)
